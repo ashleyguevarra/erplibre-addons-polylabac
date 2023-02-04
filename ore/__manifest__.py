edf3ceef05c7f3b7451c2fb9c94f48b7edd3ec4c
Mathieu Benoit <mathben@technolibre.ca>
2023-02-04 18:22:22 -0500

    [ADD] module ORE: update company information

---
+++ ore/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "ORE",
+    "category": "Website",
+    "version": "12.0.1.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "website": "https://technolibre.ca",
+    "application": True,
+    "depends": ["accorderie"],
+    "data": [
+        "data/ir_attachment.xml",
+    ],
+    "pre_init_hook": "pre_init_hook",
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+}
---
+++ ore/hooks.py
@@ -0,0 +1,52 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import base64
+import logging
+
+from odoo import SUPERUSER_ID, _, api, tools
+
+_logger = logging.getLogger(__name__)
+
+
+def pre_init_hook(cr):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+
+        company_id = env["res.partner"].browse(env.ref("base.main_company").id)
+        company_id.website = "https://ore.nuagelibre.ca"
+        company_id.name = "ORE"
+        company_id.email = "info@cimarlab.ca"
+        company_id.city = "Québec"
+        company_id.country_id = env.ref("base.ca")
+        company_id.state_id = env["res.country.state"].search(
+            [("code", "ilike", "QC")], limit=1
+        )
+
+        company_id = env["res.company"].browse(1)
+        company_id.sequence = 1
+
+        user_admin_id = env["res.partner"].browse(
+            env.ref("base.partner_root").id
+        )
+        user_admin_id.website = "https://technolibre.ca"
+        user_admin_id.name = "Mathieu Benoit"
+        user_admin_id.email = "mathieu.benoit@technolibre.ca"
+        user_admin_id.country_id = env.ref("base.ca")
+        user_admin_id.state_id = env["res.country.state"].search(
+            [("code", "ilike", "QC")], limit=1
+        )
+
+
+def post_init_hook(cr, e):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+
+        partner_id = env["res.partner"].browse(env.ref("base.main_company").id)
+
+        partner_img_attachment = env.ref(
+            "ore.ir_attachment_logo_ore_transparent_svg"
+        )
+        with tools.file_open(
+            partner_img_attachment.local_url[1:], "rb"
+        ) as desc_file:
+            partner_id.image = base64.b64encode(desc_file.read())
