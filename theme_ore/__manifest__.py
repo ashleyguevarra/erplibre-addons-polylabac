4f2a367d51b8e2c58968ec17ded6b99d982739e6
Mathieu Benoit <mathben@technolibre.ca>
2023-02-04 16:37:58 -0500

    [ADD] theme_ore

---
+++ theme_ore/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Thème projet ORE",
+    "category": "Theme",
+    "version": "12.0.1.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "sequence": 900,
+    "website": "https://technolibre.ca",
+    "depends": [
+        "website",
+        "website_theme_install",
+    ],
+    "data": [
+        "data/theme_ore_data.xml",
+        "views/theme_ore_templates.xml",
+    ],
+    "installable": True,
+}
---
+++ theme_ore/static/src/fonts/README.md
@@ -0,0 +1,3 @@
+Font Ubuntu download from https://design.ubuntu.com/font
+
+Transform TTF to WOFF with https://transfonter.org/
