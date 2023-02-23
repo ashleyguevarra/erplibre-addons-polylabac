97b6a67a0f11528cfcf99a255d8de6858d02146c
Mathieu Benoit <mathben@technolibre.ca>
2023-02-23 02:22:34 -0500

    [ADD] website_ore: change image in many section

--- website_ore/__manifest__.py
+++ website_ore/__manifest__.py
@@ -7,7 +7,11 @@
     "website": "https://technolibre.ca",
     "application": True,
     "depends": ["website", "website_accorderie"],
-    "data": ["views/website_page.xml", "data/ir_attachment.xml"],
+    "data": [
+        "views/website_page.xml",
+        "data/ir_attachment.xml",
+        "views/ir_ui_view.xml",
+    ],
     "post_init_hook": "post_init_hook",
     "installable": True,
 }
